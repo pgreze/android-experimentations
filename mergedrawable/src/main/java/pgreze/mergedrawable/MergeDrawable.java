edb638b4ea51026979f94f1fd14f1ff7eff8fa5c
pgreze <pierrick.greze@gmail.com>
2016-03-23 14:28:01 +0100

    Add animation in mergedrawable

--- mergedrawable/src/main/java/pgreze/mergedrawable/MergeDrawable.java
+++ mergedrawable/src/main/java/pgreze/mergedrawable/MergeDrawable.java
@@ -1,5 +1,10 @@
 package pgreze.mergedrawable;
 
+import android.animation.Animator;
+import android.animation.Keyframe;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
@@ -7,6 +12,8 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import java.io.ByteArrayOutputStream;
@@ -22,6 +29,8 @@ public class MergeDrawable extends ActionBarActivity {
 
     private static final int WIDTH = 640;
     private static final int HEIGHT = 640;
+
+    private ImageView img;
     private int watermarkHeight;
     private int watermarkWidth;
 
@@ -29,8 +38,13 @@ public class MergeDrawable extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_merge_drawable);
-        ImageView img = (ImageView) findViewById(R.id.imgview);
+        img = (ImageView) findViewById(R.id.imgview);
 
+        setImage();
+        animate();
+    }
+
+    private void setImage() {
         Log.i(TAG, "Hello");
         Bitmap b = Bitmap.createBitmap(WIDTH, HEIGHT, Bitmap.Config.ARGB_8888);
         Canvas c = new Canvas(b);
@@ -101,5 +115,71 @@ public class MergeDrawable extends ActionBarActivity {
         drawable.draw(canvas);
 
         return bitmap;
+    }
+
+    // Animation
+
+    private ObjectAnimator anim;
+
+    /**
+     * See http://developer.android.com/guide/topics/graphics/prop-animation.html#keyframes
+     * Or XML alternative: http://developer.android.com/reference/android/animation/ObjectAnimator.html
+     * But keyframe xml tag is enabled for at least API level 23.
+     */
+    private void animate() {
+        int seconds = 3;
+        float fraction = 1f / (12 * seconds);
+        Keyframe[] keyframes = new Keyframe[] {
+                Keyframe.ofFloat(0f, 0f),
+                Keyframe.ofFloat(fraction, 15f),
+                Keyframe.ofFloat(fraction * 3f, -15f),
+                Keyframe.ofFloat(fraction * 5f, 10f),
+                Keyframe.ofFloat(fraction * 7f, -10f),
+                Keyframe.ofFloat(fraction * 9f, 5f),
+                Keyframe.ofFloat(fraction * 11f, -5f),
+                Keyframe.ofFloat(fraction * 12f, 0f),
+                Keyframe.ofFloat(1f, 0f),
+        };
+        PropertyValuesHolder pvhRotation = PropertyValuesHolder.ofKeyframe("rotationY", keyframes);
+        anim = ObjectAnimator.ofPropertyValuesHolder(img, pvhRotation);
+        anim.setDuration(seconds * 1000);
+        anim.setRepeatCount(ValueAnimator.INFINITE);
+        anim.setRepeatMode(ValueAnimator.RESTART);
+        anim.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                Log.d(TAG, "AnimationStart(" + animation + ")");
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                Log.d(TAG, "AnimationEnd(" + animation + ")");
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                Log.d(TAG, "AnimationCancel(" + animation + ")");
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                Log.d(TAG, "AnimationRepeat(" + animation + ")");
+            }
+        });
+        anim.start();
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (anim.isRunning()) {
+                    anim.cancel();
+                    img.startAnimation(AnimationUtils.loadAnimation(
+                            getApplicationContext(), R.anim.shake));
+                } else {
+                    img.clearAnimation();
+                    anim.start();
+                }
+            }
+        });
     }
 }
